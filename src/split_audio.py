98df27bc6ab0c6cd8fc561e14ee5c525e11b2204
Stannie04 <stan.ruijters@outlook.com>
2024-04-04 00:14:23 +0200

    SPLITTING WORKS YAY

--- src/split_audio.py
+++ src/split_audio.py
@@ -1,4 +1,3 @@
-import os
 from pydub import AudioSegment
 from parse_txt import get_split_composition_files, count_notes, txt_to_string
 from constants import MXL_DIR, SPLIT_TXT_DIR, WAV_DIR
@@ -15,15 +14,17 @@ def alignments_from_csv(csv_file):
 
 def split_mp3(mp3_file, splits):
     """Split an mp3 file into segments based on the given splits."""
-    print(mp3_file)
     audio = AudioSegment.from_mp3(mp3_file)
     for i, split in enumerate(splits):
         start, end = split
+
+        start = int(float(start)*1000)
+        end = int(float(end)*1000)
+
         segment = audio[start:end]
-        segment.export(f"{WAV_DIR}/split/split_{i}.wav", format="wav")
+        segment.export(f"{WAV_DIR}/wav_split/split_{i}.wav", format="wav")
 
 def get_split_times(alignments, split_files):
-
     n_notes = [count_notes(txt_to_string(SPLIT_TXT_DIR, file)) for file in split_files]
     current_note = 0
 
@@ -41,7 +42,7 @@ def get_split_times(alignments, split_files):
 def main():
     alignments = alignments_from_csv("oliver-colbentson_bwv1006_mov1.csv")
     split_files = get_split_composition_files("bwv1006_mov1.mxl")
-    splits = get_split_times(alignments, split_files)
+    splits = get_split_times(alignments, sorted(split_files))
     split_mp3(f"{WAV_DIR}/wav_whole/oliver-colbentson_bwv1006_mov1.mp3", splits)
 
 
